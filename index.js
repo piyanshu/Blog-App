4f737e6241048dd484d34e4a1c22f6fc8a38017b
Piyanshu Mangla <piyanshumangla456@gmail.com>
2022-03-01 16:18:19 +0530

    Handling first req and res

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ const bodyParser = require('body-parser');
 const port = 8000;
 
 const app = express();
+app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', function(req, res){
+    res.send('Hello');
+})
 
 app.listen(port, function(req, res){
     console.log(`Server is running on ${port}`);
