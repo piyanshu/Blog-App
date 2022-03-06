a50e96a91edada69be1c874704378fb1a8194d57
Piyanshu Mangla <piyanshumangla456@gmail.com>
2022-03-06 21:08:54 +0530

    do some styling for index.js

--- index.js
+++ index.js
@@ -61,7 +61,15 @@ app.get('/blog/delete', function(req, res){
         }
     });
 });
-
+app.get('/show/:id', function(req, res){
+    blog.find(req.params.id, function(err, blog){
+        if(err){
+            console.log('error in finding the blog');
+        }else{
+            return res.render('show', {blog: blog});
+        }
+    });
+});
 app.listen(port, function(req, res){
     console.log(`Server is running on ${port}`);
 });
